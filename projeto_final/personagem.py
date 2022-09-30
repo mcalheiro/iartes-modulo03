b911faa0aff783075e97cb626a27a4022c41b30e
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2022-09-30 19:14:22 -0400

    Ate classe Guerreiro

---
+++ projeto_final/personagem.py
@@ -0,0 +1,87 @@
+"""
+Personagem: Heroi, Monstro
+Heroi: Mago, Guerreiro
+Monstro: Orc, Dragao
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Personagem(ABC):
+    """
+    Classe abstrata.
+    Possui nome e nivel.
+    Pode atacar ou defender.
+    """
+    def __init__(self, nome: str, nivel: int):
+        self.nome = nome
+        self.nivel = nivel
+
+    @abstractmethod
+    def atacar(self): pass
+
+    @abstractmethod
+    def defender(self): pass
+
+
+class Heroi(Personagem):
+    """
+    Possui todos os atributos de Personagem + raca.
+    Racas: humano, elfo, anao, etc.
+    """
+    def __init__(self, raca: str, nome: str, nivel: int):
+        super().__init__(nome, nivel)
+        self.raca = raca
+
+    @property
+    def raca(self):
+        return self._raca
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def nivel(self):
+        return self._nivel
+
+    @raca.setter
+    def raca(self, raca_personagem):
+        self._raca = raca_personagem
+
+    @nome.setter
+    def nome(self, nome_personagem):
+        self._nome = nome_personagem
+
+    @nivel.setter
+    def nivel(self, nivel_personagem):
+        self._nivel = nivel_personagem
+
+    def atacar(self):
+        return f'{self.nome} atacou.'
+
+    def defender(self):
+        return f'{self.nome} defendeu.'
+
+    def fugir(self):
+        return f'{self.nome} fugiu da luta.'
+
+
+class Guerreiro(Heroi):
+    """
+    Possui todos os atrivutos de Personagem e Heroi + arma e HP
+    Pode evoluir nivel e equipar outras armas.
+    Arma padrao: espada
+    HP padrao: 100
+    """
+    def __init__(self, raca: str, nome: str, nivel: int):
+        super().__init__(raca, nome, nivel)
+        self.arma = 'Espada'
+        self.hp = 100
+
+    def equipar(self, nova_arma: str):
+        armas_possiveis = ['Espada', 'Adaga', 'Arco e flecha']
+        if isinstance(nova_arma, str) and nova_arma in armas_possiveis:
+            print(f'{self.nome} equipou {nova_arma}')
+            self.arma = nova_arma
+
